737d21410819a402ae540b921ac8b796f133e084
SavvySantos <ssantos@byupathway.edu>
2025-09-10 11:30:36 -0600

    commit after corrupted files

---
+++ week03/ScriptureMemorizer/Word.cs
@@ -0,0 +1,43 @@
+// Methods
+// Hide() : void
+// Show() : void
+// IsHidden() : bool
+// GetDisplayText() : string
+
+// Attributes
+// _text : string
+// _isHidden : bool
+
+using System;
+using System.Runtime.CompilerServices;
+class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public Word()
+    {
+        _text = "27 And if men come unto me I will show unto them their weekness. I give unto men weakness that they may be humble; and my grace is sufficient for all men that humble themselves before me; for if tey humble themselves before me, and have faith in me, then will I make weak things become strong unto them. 28 Behold, I will show unto the Gentiles their weakness, and I will show unto them that faith, hope and charity bringeth unto me-- the fountain of all rightousness.";
+       // _isHidden = 
+    }
+    public void Hide()
+    {
+
+    }
+
+    public void Show()
+    {
+
+    }
+
+    public bool IsHidden()
+    {
+        return IsHidden();
+    }
+
+    public string GetDisplayText()
+    {
+        return "";
+    }
+}
+
